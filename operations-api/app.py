aed35aabc6fd119e10c2ef3706ad77d00d4ee8c0
Juanlu <juanlurasgue06@gmail.com>
2022-03-25 11:28:19 +0100

    API terminated

--- operations-api/app.py
+++ operations-api/app.py
@@ -1,3 +1,4 @@
+from decimal import DivisionByZero
 import flask
 import module_operations.operations as operations
 
@@ -6,23 +7,45 @@ app = flask.Flask(__name__)
 @app.route('/suma', methods=['POST'])
 def suma():
     data = flask.request.json
-    suma =operations.suma(data["v1"], data["v2"])
-    return str(suma)
+    try:
+        response = str(operations.suma(data["v1"], data["v2"]))
+    except TypeError:
+        response = flask.Response("Variables should be of type list.",status=500)
+    except OverflowError:
+        response = flask.Response("The lists should have the same size.",status=500)
+    return response
 
 @app.route('/resta', methods=['POST'])
 def resta():
     data = flask.request.json
-    resta =operations.resta(data["v1"], data["v2"])
-    return str(resta)
+    try:
+        response = str(operations.resta(data["v1"], data["v2"]))
+    except TypeError:
+        response = flask.Response("Variables should be of type list.",status=500)
+    except OverflowError:
+        response = flask.Response("The lists should have the same size.",status=500)
+    return response
 
 @app.route('/mult', methods=['POST'])
 def mult():
     data = flask.request.json
-    suma =operations.mult(data["v1"], data["v2"])
-    return str(mult)
+    try:
+        response = str(operations.mult(data["v1"], data["v2"]))
+    except TypeError:
+        response = flask.Response("Variables should be of type list.",status=500)
+    except OverflowError:
+        response = flask.Response("The lists should have the same size.",status=500)
+    return response
 
 @app.route('/divis', methods=['POST'])
 def divis():
     data = flask.request.json
-    divis =operations.divis(data["v1"], data["v2"])
-    return str(divis)
+    try:
+        response = str(operations.divis(data["v1"], data["v2"]))
+    except TypeError:
+        response = flask.Response("Variables should be of type list.",status=500)
+    except OverflowError:
+        response = flask.Response("The lists should have the same size.",status=500)
+    except ZeroDivisionError:
+        response = flask.Response("Division by 0 is not possible.",status=500)
+    return response
