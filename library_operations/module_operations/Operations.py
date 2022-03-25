61424835d66f67298d28730ffd3c94579f1612fc
Juanlu <juanlurasgue06@gmail.com>
2022-03-25 11:12:44 +0100

    Fixed operations library exceptions

--- library_operations/module_operations/Operations.py
+++ library_operations/module_operations/Operations.py
@@ -1,25 +1,36 @@
 def suma(a: list, b:list) -> list:
 
-    suma = [x + y for x, y in zip(a, b)]
+    if(len(a) == len(b)):
+        suma = [x + y for x, y in zip(a, b)]
+    else:
+        raise OverflowError
 
     return suma
 
 def resta(a: list, b:list) -> list:
-
-    resta = [x - y for x, y in zip(a, b)]
+    if(len(a) == len(b)):
+        resta = [x - y for x, y in zip(a, b)]
+    else:
+        raise OverflowError
 
     return resta
 
 
 def mult(a: list, b:list) -> list:
 
-    mult = [x * y for x, y in zip(a, b)]
+    if(len(a) == len(b)):
+        mult = [x * y for x, y in zip(a, b)]
+    else:
+        raise OverflowError
 
     return mult
 
 def divis(a: list, b:list) -> list:
 
-    divis = [x / y for x, y in zip(a, b)]  
+    if(len(a) == len(b)):
+        divis = [x / y for x, y in zip(a, b)]  
+    else:
+        raise OverflowError
 
     return divis
 
--- library_operations/module_operations/tests/test_operations.py
+++ library_operations/module_operations/tests/test_operations.py
@@ -1,6 +1,6 @@
 
 import unittest
-from operations import *
+from module_operations.operations import *
 
 class TestOperations(unittest.TestCase):
 
@@ -8,13 +8,17 @@ class TestOperations(unittest.TestCase):
         self.a = [6,5,9,8]
         self.b = [2,5,2,2]
 
-    def test_sum(self):
+    def test_suma(self):
         valorCalculado = suma(self.a,self.b)
         valorReal = [8, 10, 11, 10]
         self.assertEqual(valorCalculado, valorReal)
     
-    def test_sum_int(self):
+    def test_suma_int(self):
         self.assertRaises(TypeError, sum, 4,7)
+
+    def test_suma_distinct_size_list(self):
+        listDistinct = [2,3,4]
+        self.assertRaises(OverflowError, suma, self.a, listDistinct)
 
     def test_resta(self):
         valorCalculado = resta(self.a,self.b)
@@ -23,6 +27,10 @@ class TestOperations(unittest.TestCase):
 
     def test_resta_int(self):
         self.assertRaises(TypeError, resta, 4,7)
+    
+    def test_resta_distinct_size_list(self):
+        listDistinct = [2,3,4]
+        self.assertRaises(OverflowError, resta, self.a,listDistinct)
 
     def test_mult(self):
         valorCalculado = mult(self.a,self.b)
@@ -32,6 +40,10 @@ class TestOperations(unittest.TestCase):
     def test_mult_int(self):
         self.assertRaises(TypeError, mult, 4,7)
 
+    def test_mult_distinct_size_list(self):
+        listDistinct = [2,3,4]
+        self.assertRaises(OverflowError, mult, self.a, listDistinct)
+
     def test_divis(self):
         valorCalculado = divis(self.a,self.b)
         valorReal = [3, 1, 4.5, 4]
@@ -39,6 +51,10 @@ class TestOperations(unittest.TestCase):
 
     def test_divis_int(self):
         self.assertRaises(TypeError, divis, 4,7)
+
+    def test_divis_distinct_size_list(self):
+        listDistinct = [2,3,4]
+        self.assertRaises(OverflowError, divis, self.a,listDistinct)
 
     def test_divisBy0(self):
         self.b = [2,5,0,2]
