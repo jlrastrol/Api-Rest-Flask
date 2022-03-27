706c2fb82ff2bd0380390e4f72e19c3660ee3a11
Juan Luis Rastrollo Guerrero <juanlurasgue06@gmail.com>
2022-03-27 21:54:41 +0200

    List data type control.

--- library_operations/module_operations/Operations.py
+++ library_operations/module_operations/Operations.py
@@ -1,38 +1,64 @@
-def suma(a: list, b:list) -> list:
+class operations:
 
-    if(len(a) == len(b)):
-        suma = [x + y for x, y in zip(a, b)]
-    else:
-        raise OverflowError
+    def suma(self, a: list, b:list) -> list:
 
-    return suma
+        if(self.__onlyNumber(a) != True or self.__onlyNumber(b) != True ):
+            raise TypeError
 
-def resta(a: list, b:list) -> list:
-    if(len(a) == len(b)):
-        resta = [x - y for x, y in zip(a, b)]
-    else:
-        raise OverflowError
+        if(len(a) == len(b)):
+            suma = [x + y for x, y in zip(a, b)]
+        else:
+            raise OverflowError
 
-    return resta
+        return suma
 
+    def resta(self, a: list, b:list) -> list:
 
-def mult(a: list, b:list) -> list:
+        if(self.__onlyNumber(a) != True or self.__onlyNumber(b) != True ):
+            raise TypeError
 
-    if(len(a) == len(b)):
-        mult = [x * y for x, y in zip(a, b)]
-    else:
-        raise OverflowError
+        if(len(a) == len(b)):
+            resta = [x - y for x, y in zip(a, b)]
+        else:
+            raise OverflowError
 
-    return mult
-
-def divis(a: list, b:list) -> list:
-
-    if(len(a) == len(b)):
-        divis = [x / y for x, y in zip(a, b)]  
-    else:
-        raise OverflowError
-
-    return divis
+        return resta
 
 
+    def mult(self, a: list, b:list) -> list:
+
+        if(self.__onlyNumber(a) != True or self.__onlyNumber(b) != True ):
+            raise TypeError
+
+        if(len(a) == len(b)):
+            mult = [x * y for x, y in zip(a, b)]
+        else:
+            raise OverflowError
+
+        return mult
+
+    def divis(self, a: list, b:list) -> list:
+
+        if(self.__onlyNumber(a) != True or self.__onlyNumber(b) != True ):
+            raise TypeError
+
+        if(len(a) == len(b)):
+            divis = [x / y for x, y in zip(a, b)]  
+        else:
+            raise OverflowError
+
+        return divis
+
+
+    def __onlyNumber(self , a: list) -> bool:
+
+        onlyInteger = True
+        types = (float, int)
+
+        for element in a:
+            if(isinstance(element, types) != True ):
+                onlyInteger = False
+                break
+
+        return onlyInteger
 
--- library_operations/module_operations/tests/test_operations.py
+++ library_operations/module_operations/tests/test_operations.py
@@ -1,64 +1,81 @@
 
+import operator
 import unittest
-from module_operations.operations import *
-
+from operations import operations 
 class TestOperations(unittest.TestCase):
 
     def setUp(self):
         self.a = [6,5,9,8]
         self.b = [2,5,2,2]
+        self.oper = operations()
 
     def test_suma(self):
-        valorCalculado = suma(self.a,self.b)
+        valorCalculado = operations.suma(self.oper,self.a,self.b)
         valorReal = [8, 10, 11, 10]
         self.assertEqual(valorCalculado, valorReal)
     
     def test_suma_int(self):
-        self.assertRaises(TypeError, sum, 4,7)
+        self.assertRaises(TypeError, operations.suma,self.oper, 4,7)
+    
+    def test_suma_list_not_int(self):
+        listDistinct = ["1",3,"a"]
+        self.assertRaises(TypeError, operations.suma,self.oper, self.a, listDistinct)
 
     def test_suma_distinct_size_list(self):
         listDistinct = [2,3,4]
-        self.assertRaises(OverflowError, suma, self.a, listDistinct)
+        self.assertRaises(OverflowError, operations.suma, self.oper, self.a, listDistinct)
 
     def test_resta(self):
-        valorCalculado = resta(self.a,self.b)
+        valorCalculado = operations.resta(self.oper,self.a,self.b)
         valorReal = [4, 0, 7, 6]
         self.assertEqual(valorCalculado, valorReal)
 
     def test_resta_int(self):
-        self.assertRaises(TypeError, resta, 4,7)
+        self.assertRaises(TypeError, operations.resta,self.oper, 4,7)
+
+    def test_resta_list_not_int(self):
+        listDistinct = ["1",3,"a"]
+        self.assertRaises(TypeError, operations.resta,self.oper, self.a, listDistinct)
     
     def test_resta_distinct_size_list(self):
         listDistinct = [2,3,4]
-        self.assertRaises(OverflowError, resta, self.a,listDistinct)
+        self.assertRaises(OverflowError, operations.resta,self.oper, self.a,listDistinct)
 
     def test_mult(self):
-        valorCalculado = mult(self.a,self.b)
+        valorCalculado = operations.mult(self.oper,self.a,self.b)
         valorReal = [12, 25, 18, 16]
         self.assertEqual(valorCalculado, valorReal)
 
     def test_mult_int(self):
-        self.assertRaises(TypeError, mult, 4,7)
+        self.assertRaises(TypeError, operations.mult,self.oper, 4,7)
+
+    def test_mult_list_not_int(self):
+        listDistinct = ["1",3,"a"]
+        self.assertRaises(TypeError, operations.mult,self.oper, self.a, listDistinct)
 
     def test_mult_distinct_size_list(self):
         listDistinct = [2,3,4]
-        self.assertRaises(OverflowError, mult, self.a, listDistinct)
+        self.assertRaises(OverflowError, operations.mult,self.oper, self.a, listDistinct)
 
     def test_divis(self):
-        valorCalculado = divis(self.a,self.b)
+        valorCalculado = operations.divis(self.oper, self.a,self.b)
         valorReal = [3, 1, 4.5, 4]
         self.assertEqual(valorCalculado, valorReal)
 
     def test_divis_int(self):
-        self.assertRaises(TypeError, divis, 4,7)
+        self.assertRaises(TypeError, operations.divis, 4,7)
+
+    def test_divis_list_not_int(self):
+        listDistinct = ["1",3,"a"]
+        self.assertRaises(TypeError, operations.divis,self.oper, self.a, listDistinct)
 
     def test_divis_distinct_size_list(self):
         listDistinct = [2,3,4]
-        self.assertRaises(OverflowError, divis, self.a,listDistinct)
+        self.assertRaises(OverflowError, operations.divis, self.oper, self.a,listDistinct)
 
     def test_divisBy0(self):
         self.b = [2,5,0,2]
-        self.assertRaises(ZeroDivisionError, divis, self.a,self.b)
+        self.assertRaises(ZeroDivisionError, operations.divis, self.oper, self.a,self.b)
 
 if __name__ == "__main__":
     unittest.main()
